6610fe0882ad73dd52ad5943230246d83001c81c
iamprincesly <iamprincesly@gmail.com>
2021-12-16 09:44:05 +0100

    Install erroran and testing it

--- app.js
+++ app.js
@@ -2,22 +2,34 @@
  * @author Sylvanus Etim
  * @email iamprincesly@gmail.com
  * @create date 2021-12-15 17:45:54
- * @modify date 2021-12-15 17:46:26
+ * @modify date 2021-12-16 09:40:34
  * @desc Set up app and registers all middleware and routers
  */
 const express = require('express');
+const Erroran = require('erroran');
 
 const app = express();
 
 const router = express.Router();
 
-const test = router.get('/test', (req, res) => {
-    return res.status(200).json({
-        status: 'success',
-        message: 'Test successful',
-    });
+const test = router.get('/test', (req, res, next) => {
+    return next(Erroran.notAuthenticated('This is my own not authenticated'));
 });
 
 app.use('/', test);
+
+app.use(
+    '/error',
+    router.get('/', (req, res, next) => {
+        // return res.status(200).json({
+        //     'status': 'success',
+        //     'message': 'This is my own error'
+        // });
+
+        return next(Erroran.notFound('This is my own not found'));
+    })
+);
+
+app.use(Erroran.handler);
 
 module.exports = app;
