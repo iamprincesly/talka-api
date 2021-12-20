c3eff49cd9baa059c6fdd3aee192dd217e6c2760
iamprincesly <iamprincesly@gmail.com>
2021-12-20 12:04:44 +0100

    Install dotenv package, added .env file and set up environment variables

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
  * @author Sylvanus Etim
  * @email iamprincesly@gmail.com
  * @create date 2021-12-15 17:42:15
- * @modify date 2021-12-15 17:45:26
+ * @modify date 2021-12-20 12:01:34
  * @desc Spin up node express server
  */
 
+/**
+ * ----------------------------------------------------------------
+ * Importing Dependencies
+ * ----------------------------------------------------------------
+ */
+ const dotenv = require('dotenv');
+ 
 // Catch all stop server if somethings goes wrong
 process.on('uncaughtException', (err) => {
     console.log('UNCAUGHT EXCEPTION: Shutting down...');
     console.log(err.name, err.message);
     process.exit(1);
 });
+
+// Load env vars
+dotenv.config({ path: './.env' });
 
 const app = require('./app');
 
