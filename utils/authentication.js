2cc72b164d0bf82ed64b1c99c81a7117be600707
iamprincesly <iamprincesly@gmail.com>
2022-01-05 22:34:57 +0100

    Implement logout feature

--- utils/authentication.js
+++ utils/authentication.js
@@ -2,7 +2,7 @@
  * @author Sylvanus Etim
  * @email iamprincesly@gmail.com
  * @create date 2022-01-02 20:32:19
- * @modify date 2022-01-02 21:04:10
+ * @modify date 2022-01-05 22:31:21
  * @desc Create and send token to authenticate user
  */
 /**
@@ -14,10 +14,13 @@
 
  /**
   * ----------------------------------------------------------------
-  * Import this module to send access token to client
+  * Import this module to send access token to client and 
+  * store it in cookie.
   * ----------------------------------------------------------------
   */
- module.exports = (user, statusCode, req, res) => {
+
+ // Send token to authenticate user
+ exports.authenticateUser = (user, statusCode, req, res) => {
      const token = user.getSignInToken();
  
      const options = {
@@ -35,5 +38,15 @@
      res.status(statusCode)
          .cookie('jwt', token, options)
          .json({ success: true, token });
+ };
+
+ // Delete token to logout a user
+ exports.logoutUser =  (res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({ status: 'success' });
  };
  
