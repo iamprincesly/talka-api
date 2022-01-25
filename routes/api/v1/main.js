f387077d5fd0d315049b4f66e32d0ddfcd9d2e4b
iamprincesly <iamprincesly@gmail.com>
2022-01-25 20:33:58 +0100

    Register profile route and a little fit amendment

--- routes/api/v1/main.js
+++ routes/api/v1/main.js
@@ -2,7 +2,7 @@
  * @author Sylvanus Etim
  * @email iamprincesly@gmail.com
  * @create date 2021-12-16 10:03:03
- * @modify date 2022-01-02 16:23:43
+ * @modify date 2022-01-25 20:29:34
  * @desc Set up all api version 1 routes
  */
 /**
@@ -17,9 +17,10 @@ const express = require('express');
  * Importing routers
  * ----------------------------------------------------------------
  */
-const postRoute = require('./post');
-const userRoute = require('./user');
-const authRoute = require('./auth');
+const postRoute = require('./app/post');
+const accountRoute = require('./app/account');
+const profileRoute = require('./app/profile');
+const authRoute = require('./app/auth');
 
 const router = express.Router();
 
@@ -30,23 +31,50 @@ const router = express.Router();
  */
 router.get('/', function (req, res, next) {
     return res.status(200).json({
-        'status': 'success',
-        'message': 'this a successful request'
+        status: 'success',
+        message: 'this a successful request',
     });
 });
+
+/**
+ * ----------------------------------------------------------------
+ * Importing middlewares
+ * ----------------------------------------------------------------
+ */
+const { authCheck } = require('../../../../middlewares/auth_middleware');
 
 /**
  * ----------------------------------------------------------------
  * Mounting post route
  * ----------------------------------------------------------------
  */
-// Post related router
+
+/**
+ * User account related router
+ * 
+ * @access public
+ */
 router.use('/posts', postRoute);
 
-// User related router
-router.use('/users', userRoute);
+/**
+ * User account related router
+ * 
+ * @access private
+ */
+router.use('/account', authCheck, accountRoute);
 
-// All Authentication related router
+/**
+ * Get any user public profile through their username
+ * 
+ * @access public
+ */
+router.use('/profile', profileRoute);
+
+/**
+ * Authentication account related router
+ * 
+ * @access public
+ */
 router.use('/auth', authRoute);
 
 module.exports = router;
